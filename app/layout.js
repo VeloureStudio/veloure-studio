d758c66b9f7342a085362024c44a616006d66274
VeloureStudio <velourestudio.cor@gmail.com>
2026-08-18 00:52:31 +0800

    Add RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Veloure Studio',
+  description: 'Digital Assets & Mockups',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+} 
